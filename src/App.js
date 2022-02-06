e299892a86d27723a990c6b4af5120df48238cbf
Carlos <carlosteste360@gmail.com>
2022-02-06 10:24:29 -0300

    Importando componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Categories from './components/Categories';
+import Choose from './components/Choose';
+import Home from './components/Home';
+import MostSold from './components/MostSold';
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+import Promo from './components/Promo';
+import Recommend from './components/Recommend';
+import ScrollToTop from './components/ScrollToTop';
+import Services from './components/Services';
+import "./scss/index.scss";
 export default function App() {
-  return <div>Teste</div>;
+  const [theme, setTheme] = useState("dark");
+  return <div className="app" data-theme={theme}>
+    <Categories />
+    <Choose />
+    <Home />
+    <MostSold />
+    <Navbar />
+    <Products />
+    <Promo />
+    <Recommend />
+    <ScrollToTop />
+    <Services />
+  </div>;
 }
